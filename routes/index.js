daf02cec1fe0f4d0e16bfb56413585019e83b169
Harsh Narayan <narayanharsh2000@gmail.com>
2023-01-19 18:32:04 +0530

    created base

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: "Message Board", messages});
+})
+
+router.post('/new', function(req, res, next) {
+  messages.push(
+    {
+      text: req.body.msg,
+      user: req.body.author,
+      added: new Date(),
+    }
+  )
+
+  res.redirect('/');
+})
 
 module.exports = router;
